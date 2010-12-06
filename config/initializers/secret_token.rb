ca3bb69b6fdb939b2e95a031706e2bb4b2b83ddc
Felix Braun <mail@felixbraun.com>
2010-12-06 19:50:19 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersoSP::Application.config.secret_token = 'de6c0e7156df3547f50d6c6805555001d81da12af733bd60d6af308c460ff2a4cc2d540756982756d5ebc6a5885aeca1ffff4b044f53ed76679df3c7ffd853c9'
