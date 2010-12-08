4dcf9abe56641db0f75fcd196b9816b3d8bacdf2
Felix Braun <mail@felixbraun.com>
2010-12-08 22:38:36 +0100

    Added a site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,18 +1,49 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
   end
+  
+  it "should have the right title" do
+    get 'home'
+    response.should have_selector("title",
+          :content => "PersoSP - Personnel Services Procurement | Home")
+  end
+  
+  it "should have a non-blank body" do
+    get 'home'
+    response.body.should_not =~ /<body>\s*<\/body>/
+  end 
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", 
+            :content => "PersoSP - Personnel Services Procurement | Contact")
+    end
+  end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+            :content => "PersoSP - Personnel Services Procurement | About")
     end
   end
 
